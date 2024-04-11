4affa90dbf318d167e3e24f620358f1a8cbe8d5e
alexvk <alex@kavlab.ru>
2024-04-11 22:36:34 +0400

    add pipeline

--- lab01/pipeline.sh
+++ lab01/pipeline.sh
@@ -1,3 +1,13 @@
 #!/bin/bash
 
+# генерация данных
 python ./data_creation.py
+
+# предобработка данных
+python ./model_preprocessing.py
+
+# обучение модели
+python ./model_preparation.py
+
+# тестирование модели
+python ./model_testing.py
